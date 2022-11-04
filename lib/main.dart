922c186d0bbc1dd5754284a2f120e50a94be19ae
Alexandre Rodrigues <ale.rodrigues@outlook.com>
2022-11-04 00:23:12 -0300

    [15] MICRO-FRONTENDS: REFATORAÇÃO DO MOVIEAPP ​ Refatoração do movieapp criado no curso de Flutter Básico para aplicação da nova arquitetura de micro-frontends

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:micro_core/micro_core.dart';
+import 'package:movie/movie.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget with BaseApp {
+  MyApp({super.key}) {
+    super.registerInjections();
+    super.registerRouters();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MovieApp',
+      theme: ThemeData.dark(),
+      navigatorKey: navigatorKey,
+      onGenerateRoute: super.generateRoute,
+      initialRoute: '/home',
+    );
+  }
+
+  @override
+  Map<String, WidgetBuilderArgs> get baseRoutes => {};
+
+  @override
+  List<MicroApp> get microApps => [
+        MicroAppMovieResolver(),
+      ];
+}
---
+++ packages/micro_apps/movie/lib/app/features/movie/data/datasources/local/get_movies_local_datasource_decorator_imp.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+
+import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../get_movies_datasource.dart';
+import 'get_movies_datasource_decorator.dart';
+import '../../dtos/movie_dto.dart';
+import '../../../domain/entities/movie_entity.dart';
+
+class GetMoviesLocalDataSourceDecoratorImp
+    extends GetMoviesDataSourceDecorator {
+  GetMoviesLocalDataSourceDecoratorImp(GetMoviesDataSource getMoviesDataSource)
+      : super(getMoviesDataSource);
+
+  @override
+  Future<Either<Exception, MovieEntity>> call() async {
+    return (await super()).fold(
+      (error) async => Right(await _getInCache()),
+      (result) {
+        _saveInCache(result);
+        return Right(result);
+      },
+    );
+  }
+
+  _saveInCache(MovieEntity movies) async {
+    var prefs = await SharedPreferences.getInstance();
+    String jsonMovies = jsonEncode(movies.toJson());
+    prefs.setString('movies_cache', jsonMovies);
+    if (kDebugMode) {
+      print('salvou no cache ');
+    }
+  }
+
+  Future<MovieEntity> _getInCache() async {
+    var prefs = await SharedPreferences.getInstance();
+    var moviesJsonString = prefs.getString('movies_cache')!;
+    var json = jsonDecode(moviesJsonString);
+    var movies = MovieDto.fromJson(json);
+    if (kDebugMode) {
+      print('recuperou do cache os filmes ' + movies.toString());
+    }
+    return movies;
+  }
+}
